69bf8fc248fcf4114b2401ac8d377bccd4ea85c1
leoaltemari <leo.altemari@gmail.com>
2023-04-02 18:49:18 -0300

    Adding new paths shortcuts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+import path from 'path';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+
 export default defineConfig({
-  plugins: [react()],
-})
+  base: './',
+  resolve: {
+    alias: {
+      Components: path.resolve(__dirname, './src/components'),
+      Assets: path.resolve(__dirname, './src/assets'),
+    },
+  },
+  plugins: [react(), tsconfigPaths()],
+});
