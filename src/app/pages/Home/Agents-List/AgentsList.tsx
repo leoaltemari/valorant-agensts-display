e4e9bfd921bbefff1ba064637065b9155343850e
leoaltemari <leo.altemari@gmail.com>
2023-04-08 19:38:02 -0300

    Improved agents search with useMemo

--- src/app/pages/Home/Agents-List/AgentsList.tsx
+++ src/app/pages/Home/Agents-List/AgentsList.tsx
@@ -1,30 +1,23 @@
 
 
-import { useEffect, useState } from 'react';
+import { useMemo, useState } from 'react';
 
-import Input from '@components/Input/Input';
 import Loading from '@components/Loading/Loading';
-import { Agent } from '@models';
 import { getAgents } from '@services';
 
 import AgentCard from './Agent-Card/AgentCard';
+import './AgentsList.scss';
 
 
 export default function AgentsList() {
-  const [ filteredAgents, setFilteredAgents ] = useState<Agent[] | undefined>([]);
+  const { data: agentsList, isFetching: loading } = getAgents();
+  const [query, setQuery] = useState("");
 
-  const { data: agentsList, error, isFetching: loading } = getAgents();
+  const filteredAgents = useMemo(() => {
+    const searched = query.toLowerCase();
 
-  useEffect(() => {
-    setFilteredAgents(agentsList);
-  }, [agentsList]);
-
-  const searchForAgents = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    const search = event.target.value;
-    const filteredData = agentsList?.filter(agent => agent.displayName.toLowerCase().includes(search));
-
-    setFilteredAgents(filteredData);
-  }
+    return agentsList?.filter(agent => agent.displayName.toLowerCase().includes(searched));
+  }, [query, agentsList])
 
   const getPageContent = () => {
     return filteredAgents?.length
@@ -34,7 +27,21 @@ export default function AgentsList() {
 
   return (
     <div className="d-flex flex-column align-items-center">
-      <Input onChangeEvent={searchForAgents}/>
+      <div className="search-agent position-relative">
+        <input
+          type="text"
+          className="search-agent p-2 my-5"
+          placeholder="Search for agent..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+
+        <img
+          className="position-absolute"
+          src="/src/assets/images/icons/search.png"
+          alt="Search icon"
+        ></img>
+      </div>
 
       <div className="d-flex flex-wrap justify-content-center align-items-center mt-5">
         {
